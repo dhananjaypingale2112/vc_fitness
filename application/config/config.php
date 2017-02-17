b5d4cf07c05bc0f3926d7ca0ea35bc3b0177960a
nil1990 <nlshsuryavanshi@gmail.com>
2017-02-17 23:42:50 +0530

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/vc_fitness';
+$config['base_url'] = 'http://localhost/vc_fitness.git';
 
 /*
 |--------------------------------------------------------------------------
